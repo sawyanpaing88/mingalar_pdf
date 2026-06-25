e0f51af16e74baef7f0ea08f881cd294f36df20b
sawyanpaing88 <54151791+sawyanpaing88@users.noreply.github.com>
2026-06-26 01:48:40 +0630

    Update app.py

--- app.py
+++ app.py
@@ -1181,7 +1181,7 @@ elif page_selection == "➕ Build New Quotation Module":
     position: fixed;
     top: 50%;
     left: 50%;
-    transform: translate(-50%, -50%);
+    transform: translate(-50%, -50%); rotate(-45deg);
     opacity: 0.05; /* Adjust between 0.05 and 0.2 for best results */
     z-index: -1000; /* Ensures it stays behind text */
     width: 400px;   /* Adjust width as needed */
